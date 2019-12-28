fb2d1e6011f11e0a3c770c7ba3f0ca65dd52ec10
LukasTsunami <prateadow@gmail.com>
2019-12-27 22:52:07 -0300

    Configurando o express pra aceitar json como payload padrão e fazendo o primeiro post

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 const users = ["Diego", "Cláudio", "Victor", "Luana"];
 
 server.get("/users", (req, res) => {
@@ -16,6 +18,14 @@ server.get("/users/:index", (req, res) => {
   res.json({
     user: `${users[index]}!`
   });
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  res.json(users);
 });
 
 server.listen(3000);
